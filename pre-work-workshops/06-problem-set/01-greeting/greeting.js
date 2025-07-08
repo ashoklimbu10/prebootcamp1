5ff988350ddaa24278586125c746016db6de728d
ashoklimbu10 <limashok212@gmail.com>
2025-07-08 18:33:08 +0545

    solve do yo play

--- pre-work-workshops/06-problem-set/01-greeting/greeting.js
+++ pre-work-workshops/06-problem-set/01-greeting/greeting.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function greeting(firstName){
+if (firstName){
+    return ("Hello "+firstName+ '!')
+}
+else{
+    return 'Hello!'
+}
+}
+greeting();
+greeting('GIta');
--- pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function doYouPlayTheTheremin(a){
+    if (a[0]==='s'|| a[0]=== "S"){
+        return true;
+    }
+else{
+    return false;
+}
+}
