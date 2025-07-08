04cddb382c1fa233810dadc7ef8c0f501ded3401
ashoklimbu10 <limashok212@gmail.com>
2025-07-08 17:48:28 +0545

    default greeting solved

--- pre-work-workshops/05-functions/02-default-greet/default-greet.js
+++ pre-work-workshops/05-functions/02-default-greet/default-greet.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function defaultGreet(firstName , lastName){
+if(lastName===undefined){
+    return ('Hi '+firstName+' Doe!');
+
+}
+else{
+    return ("Hi "+firstName+' '+lastName+ '!');
+}
+}
