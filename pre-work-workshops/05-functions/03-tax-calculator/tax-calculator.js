d04e627be780cc9b5a770998b7adc63f59c66f13
ashoklimbu10 <limashok212@gmail.com>
2025-07-08 17:59:26 +0545

    solve tax calculator

--- pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function taxCalculator(amount,state){
+if (state==="NY"){
+    return amount*1.04;
+}
+else if(state==="NJ"){
+    return amount*1.06625;
+}
+else {
+    return amount;
+}
+}
