dc2d7d2a409e9412f5a8ec8e7700f0cdded05e09
ashoklimbu10 <limashok212@gmail.com>
2025-07-08 18:46:52 +0545

    solve graetest one

--- pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function maxOfThree(a,b,c){
+    if (a>b && a>c){
+        return a;
+    }
+    else if(b>a && b>c){
+        return b;
+    }
+    else{
+        return c;
+    }
+}
