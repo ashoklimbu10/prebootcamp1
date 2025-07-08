20d5d96184c0ef1581e7870204ce2f49a11f8a22
ashoklimbu10 <limashok212@gmail.com>
2025-07-08 18:15:14 +0545

    solve mnemonic

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function myMnemonic(a,b,c,d){
+let result = '';
+if(a) result += a[0];
+if(b) result += b[0];
+if(c) result += c[0];
+if(d) result += d[0];
+return result;
+}
+console.log(myMnemonic('Must', 'Accept', 'Sugar', 'Happiness'));
