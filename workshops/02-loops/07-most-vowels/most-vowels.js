8a62f05ec5cb76ad243b54a78db99e2fee0a1de1
Hari Shankar Limbu <67302527+ashoklimbu10@users.noreply.github.com>
2025-07-09 21:18:13 +0545

    solve vowel problem

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+function mostVowels(s){
+    let str=s.split(' ');
+    let vowels='aeiouAEIOU';
+    let maxVowelCount=0;
+    let resultWord='';
+for (let i=0;i<str.length;i++){
+    let currentWord=str[i];
+    let vowelCount=0;
+for (let j=0; j<currentWord.length;j++){
+    let currentChar=currentWord[j];
+    if(vowels.includes(currentChar)){
+        vowelCount++;
+    }
+  }
+  if(maxVowelCount<=vowelCount){
+    maxVowelCount=vowelCount;
+    resultWord=currentWord;
+ }
+ }
+ return resultWord;
+}
