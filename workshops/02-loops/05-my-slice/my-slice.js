ffdd5c75635f3a1c70a62f9fe268b142a8d5db17
ashoklimbu10 <limashok212@gmail.com>
2025-07-09 13:57:24 +0545

    solve slice

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function mySlice(originalString, startIdx, endIdx) {
+    let result = '';
+    if (startIdx === undefined) {
+        startIdx = 0;
+    }
+    if (endIdx === undefined) {
+        endIdx = originalString.length;
+    }
+    for (let i = startIdx; i < endIdx; i++) {
+        result += originalString[i];
+    }
+
+    return result;
+}
+console.log(mySlice('ashok',1,3));
