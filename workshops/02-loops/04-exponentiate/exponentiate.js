59af6cf6cc5e8679317f34d54000ee47eaebf89f
ashoklimbu10 <limashok212@gmail.com>
2025-07-09 13:18:41 +0545

    solve exponentiate

--- workshops/02-loops/04-exponentiate/exponentiate.js
+++ workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function exponentiate(base, power) {
+    if (power > 0) {
+        return base ** power;
+    } else if (power === 0) {
+        return 1;
+    } else {
+        return "Invalid power or base";
+    }
+}
