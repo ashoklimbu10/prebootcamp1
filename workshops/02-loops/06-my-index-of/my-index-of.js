4c83c8f29608ea1f171cd4edba563845e59d4722
Hari Shankar Limbu <67302527+ashoklimbu10@users.noreply.github.com>
2025-07-09 21:20:06 +0545

    solve indexof

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function myIndexOf(source , searchValue,startIdx=0 ){
+for (i=startIdx;i<source.length;i++){
+    let current=source.slice(i,i+searchValue.length)
+    if(current===searchValue){
+        return i;
+    }
+}
+return -1; 
+}
