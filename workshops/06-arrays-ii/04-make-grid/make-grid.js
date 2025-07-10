0f930c852ed04f82f47af2d1e4f0d72846691dd9
Hari Shankar Limbu <67302527+ashoklimbu10@users.noreply.github.com>
2025-07-10 13:30:29 +0545

    solve 2d grid problem

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function makeGrid(rows, columns) {
+    let result = [];
+    for (let i = 0; i <columns ; i++) {
+        let row = [];
+        for (let j = 1; j <=rows; j++) {
+            row.push(j);
+        }
+        result.push(row);
+    }
+
+    return result;
+}
