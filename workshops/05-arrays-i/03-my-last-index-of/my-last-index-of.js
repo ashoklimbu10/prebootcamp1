8613f2a833b01e5a89a42cbc4ae6d34cd72a296b
Hari Shankar Limbu <67302527+ashoklimbu10@users.noreply.github.com>
2025-07-10 11:44:48 +0545

    final commit

--- workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function myLastIndexOf(array,searchValue,startIdx){
+    if (!array.includes(searchValue)){
+        return -1
+    }
+    if (startIdx === undefined || startIdx >= array.length) {
+    startIdx = array.length - 1;
+  }
+  for (let i=startIdx; i >= 0; i--) {
+    if (array[i] === searchValue) {
+      return i;
+    }
+  }
+return -1;
+}
