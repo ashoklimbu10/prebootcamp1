5cc0a6bd449ad3db5ecb188907d63b2195785ed5
Hari Shankar Limbu <67302527+ashoklimbu10@users.noreply.github.com>
2025-07-10 11:11:44 +0545

    final commit

--- workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function oddCouple(arr){
+    let result=[];
+for (let i=0;i<arr.length;i++){
+    if(arr[i]%2===1){
+        result.push(arr[i]);
+    }
+    if (result.length===2){
+        break;
+    }
+}
+return result;
+}
