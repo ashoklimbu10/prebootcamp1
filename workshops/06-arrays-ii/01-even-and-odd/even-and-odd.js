40827aaecc39bb4288dffad768aedefaa39d61c7
Hari Shankar Limbu <67302527+ashoklimbu10@users.noreply.github.com>
2025-07-10 12:07:24 +0545

    solved odd even seperator

--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function evenOdd(arr){
+    let oddNum=[];
+    let evenNum=[];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]%2===0){
+            evenNum.push(arr[i]);
+        }
+        else{
+            oddNum.push(arr[i]);
+        }
+    
+    return {
+    even: evenNum,
+    odd: oddNum
+  };
+}
