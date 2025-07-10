1467b6fb12ef790aac144e14816e8a8438a8e96b
Hari Shankar Limbu <67302527+ashoklimbu10@users.noreply.github.com>
2025-07-10 12:27:54 +0545

    solved arrayflattner

--- workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function arrayFlattener(arr){
+    let result=[];
+    for (let i=0;i<arr.length;i++){
+        let current= arr[i];
+        if(Array.isArray(current)){
+            for (j=0;j<current.length;j++){
+                result.push(current[j]);
+            }
+        }
+         else {
+                result.push(current);
+            }
+    }
+    return result;
+}
