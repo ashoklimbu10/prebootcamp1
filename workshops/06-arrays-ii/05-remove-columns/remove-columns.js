ecdcd967a515bafba7a2aac3674a742058ab631f
ashoklimbu10 <limashok212@gmail.com>
2025-07-10 14:06:35 +0545

    solve remove coulums

--- workshops/06-arrays-ii/05-remove-columns/remove-columns.js
+++ workshops/06-arrays-ii/05-remove-columns/remove-columns.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function removeColumns(originalGrid, numColumns) {
+  let result = [];
+  for (let i = 0; i < originalGrid.length; i++) {
+    let row = originalGrid[i];
+    let newRow = [];
+
+    for (let j = 0; j < row.length - numColumns; j++) {
+      newRow.push(row[j]);
+    }
+
+    result.push(newRow);
+  }
+
+  return result;
+}
