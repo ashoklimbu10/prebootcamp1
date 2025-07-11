7f05e8ad51107c726d32d0d9d3c29a1972b55473
ashoklimbu10 <limashok212@gmail.com>
2025-07-11 18:32:19 +0545

    solved

--- workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
+++ workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function finderFunction(array,callback){
+for(i=0;i<array.length;i++){
+    if(callback(array[i])){
+
+    return i;
+    }
+}
+ return -1;
+}
+let numbers = [1, 3, 5, 64, 7, 12];
+let odds = [9, 13, 15, 17];
+
+function isEven(num) {
+  return !(num % 2);
+}
+
+console.log(finderFunction(numbers, isEven)) // 3
