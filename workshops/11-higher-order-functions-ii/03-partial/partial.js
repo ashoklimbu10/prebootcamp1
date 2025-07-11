11acb672e437bad9fa05fa1f6b771dcd8d2377b7
ashoklimbu10 <limashok212@gmail.com>
2025-07-11 21:23:39 +0545

    solved partial

--- workshops/11-higher-order-functions-ii/03-partial/partial.js
+++ workshops/11-higher-order-functions-ii/03-partial/partial.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function partial(callback, arg) {
+  return function (newArg) {
+    return callback(arg, newArg);
+  };
+}
+function summer(a, b) {
+  return a + b;
+}
+
+let sumFive = partial(summer, 5);
+sumFive(10); // => 15;
