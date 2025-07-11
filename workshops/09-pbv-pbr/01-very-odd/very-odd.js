43bd6f5a37c3b80ad19918ca2b2d409fb7fc4aaf
ashoklimbu10 <limashok212@gmail.com>
2025-07-11 11:21:20 +0545

    solved

--- workshops/09-pbv-pbr/01-very-odd/very-odd.js
+++ workshops/09-pbv-pbr/01-very-odd/very-odd.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function veryOdd(arr){
+let result=[];
+for (let i=0;i<arr.length;i++){
+    if(arr[i]%2==1){
+        result.push(arr[i]);
+    }
+}
+return result;
+}
+let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
+let oddNums = veryOdd(allTheNums);
+
+console.log('oddNums:', oddNums); // [1, 3, 5, 7];
+console.log('allTheNums:', allTheNums); // [1, 2, 3, 4, 5, 6, 7, 8]
