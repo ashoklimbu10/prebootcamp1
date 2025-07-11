b903771e73c5961ad7bc866d2735f1b208525a1e
ashoklimbu10 <limashok212@gmail.com>
2025-07-11 21:05:20 +0545

    solved times table

--- workshops/11-higher-order-functions-ii/02-times-table/times-table.js
+++ workshops/11-higher-order-functions-ii/02-times-table/times-table.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function timesTable(num1){
+    function product(num2){
+        return num1 * num2
+    }
+    return product;
+}
+let ninesTable = timesTable(9);
+ninesTable(8)    // => 72
+
+let twelvesTable = timesTable(12);
+twelvesTable(100) 
