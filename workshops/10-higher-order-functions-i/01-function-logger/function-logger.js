a6adbbbfac183457823b203ba87a834db6bdd798
ashoklimbu10 <limashok212@gmail.com>
2025-07-11 18:15:15 +0545

    solved function logger

--- workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
+++ workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+
+function functionLogger(func, arg) {
+  console.log("Function starting");
+  const result = func(arg);
+  console.log("Function complete");
+  return result;
+}
+function squareNum(x) {
+  return x * x;
+}
+
+let squareOfFour = functionLogger(squareNum, 4);
+// Function starting
+// Function complete
+
+console.log(squareOfFour)    // 16
