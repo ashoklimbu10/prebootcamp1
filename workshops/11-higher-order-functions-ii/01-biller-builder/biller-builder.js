37e81e9182e9b800536b3049e1363399cb2caaa6
ashoklimbu10 <limashok212@gmail.com>
2025-07-11 20:33:04 +0545

    solved biller builder

--- workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
+++ workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function billerBuilder(arr){
+function biller(num){
+    if (arr=='NY'){
+        return  num* 1.03 * 1.04;
+    }
+    else if (arr=='NJ'){
+        return num*1.05*1.06625;
+    }
+}
+    return biller;
+}
+let newYorkBiller = biller('NY');
+newYorkBiller(100) // => 107.12 (100 * 1.03 * 1.04)
+
+let newJersBiller = biller('NJ');
+newJersBiller(100) // => 111.95625 (100 * 1.05 * 1.06625)
+ 
