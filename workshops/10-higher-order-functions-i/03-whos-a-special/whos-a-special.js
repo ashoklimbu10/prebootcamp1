03c919bdaefb6ff6822c4481641ce7f68cd3ed8f
ashoklimbu10 <limashok212@gmail.com>
2025-07-11 19:08:56 +0545

    solved whosispecial

--- workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,27 @@
 // YOUR CODE BELOW
+function whosASpecial(array) {
+  let result=''
+  for (let i = 0; i < array.length; i++) {
+    
+    result+=`${array[i].name} the ${array[i].species} is very special!`;
+  }
+  return result.trim();
+}
+
+let specialPets = [
+  {
+    name: "Sadie",
+    species: "cat",
+  },
+  {
+    name: "Layla",
+    species: "cat",
+  },
+  {
+    name: "Bogie",
+    species: "dog",
+  },
+];
+
+
+console.log(whosASpecial(specialPets));
