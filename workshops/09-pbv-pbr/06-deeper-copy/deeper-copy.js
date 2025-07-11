fecc48ffc4b5c9b1dcbd27138d7ae94543aa2ab7
ashoklimbu10 <limashok212@gmail.com>
2025-07-11 14:08:58 +0545

    solved

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function deeperCopy(arr) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        inner.push(arr[i][j]);
+      }
+      result.push(inner);
+    } else {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+}
