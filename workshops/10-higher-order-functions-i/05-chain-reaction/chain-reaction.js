daa1cc61930d7cd30830289e7aee8c30ebc5bcf8
ashoklimbu10 <limashok212@gmail.com>
2025-07-11 20:01:48 +0545

    solved chain reaction

--- workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
+++ workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+function chainReaction(startVal, funcArray) {
+  let result = startVal;
+  for (let i = 0; i < funcArray.length; i++) {
+    result = funcArray[i](result);
+  }
+  return result;
+}
+function addTen(num) {
+  return num + 10;
+}
+
+function subtractFive(num) {
+  return num - 5;
+}
+
+function multiplyFive(num) {
+  return num * 5;
+}
+
+chainReaction(0, [addTen, subtractFive, multiplyFive]); // => 25
+chainReaction(0, [subtractFive, multiplyFive, addTen]); 
