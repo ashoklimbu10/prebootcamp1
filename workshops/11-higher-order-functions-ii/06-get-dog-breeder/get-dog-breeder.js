22a75daf3f9a8e7d903b1c4e95534984b40a64e4
ashoklimbu10 <limashok212@gmail.com>
2025-07-12 14:38:22 +0545

    solve getdogbredder

--- workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
+++ workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function getDogBreeder(defaultName = 'Steve', defaultAge = 0) {
+  function dogBreeder(name = defaultName, age = defaultAge) {
+    return {
+      name: name,
+      age: age
+    };
+  }
+  return dogBreeder;
+}
+let puppyFarm = getDogBreeder('Snoopy', 0);
+let rescueShelter = getDogBreeder('Odie', 3);
+
+puppyFarm('Olaf', 3); // => {name: 'Olaf', age: 3};
+puppyFarm(2); // => {name: 'Snoopy', age: 2}
+
+console.log(rescueShelter()); // => {'Odie', 3}
