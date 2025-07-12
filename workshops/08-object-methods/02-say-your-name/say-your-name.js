ae5b1ac3b99f51a3c61d5620a0b823e8a6a62644
ashoklimbu10 <limashok212@gmail.com>
2025-07-12 15:59:58 +0545

    solved

--- workshops/08-object-methods/02-say-your-name/say-your-name.js
+++ workshops/08-object-methods/02-say-your-name/say-your-name.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+let me2 = {
+  name: 'Tarana',
+  getGreeting: function(person) {
+    return `Hi ${person.name}, my name is ${this.name}.`;
+  }
+};
+
+let you = {
+  name: 'Alyssa'
+};
+
+console.log(me2.name);            // 'Tarana'
+console.log(you.name);            // 'Alyssa'
+console.log(me2.getGreeting(you)); // 'Hi Alyssa, my name is Tarana.'
