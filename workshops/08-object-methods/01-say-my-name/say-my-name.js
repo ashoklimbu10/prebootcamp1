87bb17dcadf4835a50859106314bdaac2e193fa3
ashoklimbu10 <limashok212@gmail.com>
2025-07-12 15:44:40 +0545

    solved say my greeting

--- workshops/08-object-methods/01-say-my-name/say-my-name.js
+++ workshops/08-object-methods/01-say-my-name/say-my-name.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+let me = {
+  name: "Kat",
+  getGreeting: function () {
+    return `Hi, my name is ${me.name}.`;
+  },
+};
+
+console.log(me.name); // 'Kat'
+console.log(me.getGreeting()); // 'Hi, my name is Kat.'
