584579747cc5e96390cf654abb0c616c84f306b0
ashoklimbu10 <limashok212@gmail.com>
2025-07-12 20:23:08 +0545

    solved tictactoe

--- workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+let ticTacToe = {
+  board: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+
+  move(player, row, column) {
+    
+    if (this.board[row][column] === null) {
+      this.board[row][column] = player;
+    }
+   
+    return this.board;
+  },
+
+  clear() {
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[i].length; j++) {
+        this.board[i][j] = null;
+      }
+    }
+    return this.board;
+  },
+};
