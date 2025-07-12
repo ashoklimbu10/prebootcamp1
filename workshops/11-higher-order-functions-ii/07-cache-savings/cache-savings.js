b0c5e8b1575afef052376db52ea70ee63081ae21
ashoklimbu10 <limashok212@gmail.com>
2025-07-12 15:08:41 +0545

    solved cache saving

--- workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
+++ workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function cacheSaving(callback) {
+  let cache = {};
+  function newFunction(arg1) {
+    if (cache.hasOwnProperty(arg1)) {
+      return cache[arg1];
+    }
+    let result = callback(arg1);
+    cache[arg1] = result;
+
+    return result;
+  }
+  return newFunction;
+}
